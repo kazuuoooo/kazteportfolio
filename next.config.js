1becd11634ed79edde5c9f6e5cb5cd23700722eb
kazuuoooo <97545410+kazuuoooo@users.noreply.github.com>
2023-02-24 00:17:13 -0800

    finally works

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? '/<imkazte>' : '',
-  basePath: isProd ? '/<imkazte>' : '',
+  assetPrefix: isProd ? 'imkazte' : '',
+  basePath: isProd ? 'imkazte' : '',
   webpack: (config) => {
     config.module.rules.push(
       {
