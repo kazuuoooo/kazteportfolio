fc2585a2b5f60ab6e71f8eb9a833e1e011595d06
kazuuoooo <97545410+kazuuoooo@users.noreply.github.com>
2023-02-24 15:22:42 -0800

    dsafdsa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? 'imkazte' : '',
-  basePath: isProd ? 'imkazte' : '',
   webpack: (config) => {
     config.module.rules.push(
       {
